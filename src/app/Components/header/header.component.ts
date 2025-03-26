d935c303be7603136cea5bdd5c7a20d730a680ff
Anshul Mokhale <anshulmokhale@Anshuls-MacBook-Air.local>
2025-03-26 14:01:53 +0530

    updated header component and implemented sidebar

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,12 +1,17 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule],
+  imports: [RouterModule, NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  sidebarOpen = false;
 
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
 }
